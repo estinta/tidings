d8ed49407fe4cca6a155326f9d7c3d3b508e6b61
Dan McGee <dpmcgee@gmail.com>
2010-06-11 14:32:55 -0500

    Add logout view

--- urls.py
+++ urls.py
@@ -5,6 +5,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     (r'^accounts/login/$', 'django.contrib.auth.views.login'),
+    (r'^accounts/logout/$', 'django.contrib.auth.views.logout'),
 
     (r'^admin/', include(admin.site.urls)),
 )
