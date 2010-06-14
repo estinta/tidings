841c80b16d2754befa10317cd7e2a2e3fb384d29
Dan McGee <dpmcgee@gmail.com>
2010-06-14 17:31:19 -0500

    Add scanner utils
    Used for fetching the various pieces of data from our different sources.

---
+++ scanner/utils.py
@@ -0,0 +1,44 @@
+from . import Finviz
+from . import StockLookup
+from . import StockNews
+
+from .models import News, Stock
+
+def fetch_news():
+    for stock in Stock.objects.filter(news__isnull=True):
+        print stock.ticker
+        for source, news_items in StockNews.get_news(stock.ticker).items():
+            for item in news_items:
+                news = News()
+                news.stock = stock
+                news.source = source
+                for key, value in item.items():
+                    # description, title, link, pub_date
+                    news.__setattr__(key, value)
+                news.save()
+
+def fetch_ibd():
+    StockLookup.login()
+    for stock in Stock.objects.filter(ibd_industry_rank=''):
+        print stock.ticker
+        response = StockLookup.get_stock(stock.ticker)
+        try:
+            stock_dict = StockLookup.parse_stock(response)
+        except Exception, e:
+            continue
+        stock.ibd_industry_rank = stock_dict['industry_group_rank']
+        stock.ibd_earnings_due_date = stock_dict['earnings_due_date']
+        stock.company_name = stock_dict['company_name']
+        stock.ibd_industry_group = stock_dict['industry_group']
+        stock.ibd_earnings_percentage_lastquarter = stock_dict[
+                'eps_perc_change']
+        stock.ibd_sales_percentage_lastquarter = stock_dict[
+                'sales_perc_change']
+        stock.ibd_eps_rank = stock_dict['eps_rating']
+        stock.save()
+
+def fetch_float():
+    for stock in Stock.objects.filter(float=''):
+        stock.float = Finviz.get_float(stock.ticker)
+        stock.save()
+
