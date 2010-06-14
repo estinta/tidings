1999f474a6eb3a5f04d971991830f7eca1962d14
Dan McGee <dpmcgee@gmail.com>
2010-06-14 17:30:10 -0500

    Add StockLookup component
    Username (email) and password come from the Django settings, which will
    incorporate a private_settings.py file for these two values.

---
+++ scanner/StockLookup.py
@@ -0,0 +1,68 @@
+from django.conf import settings
+
+import urllib2
+import urllib
+import cookielib
+import lxml.html
+import re
+import datetime
+
+LOGIN_URL = "http://www.investors.com/Services/SiteAjaxService.asmx/MemberSingIn"
+URL = "http://www.investors.com/StockResearch/StockCheckup.aspx?symbol="
+USER_AGENT = "Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.1.2) Gecko/20090729 Firefox/3.5.2"
+
+HEADERS={'User-Agent': USER_AGENT}
+
+cookiejar = cookielib.LWPCookieJar()
+opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookiejar))
+urllib2.install_opener(opener)
+
+COMPANY_GROUP_RE = re.compile('(.*) RANK WITHIN THE (.*) GROUP \(\d+ STOCKS\)')
+
+def login():
+    req = urllib2.Request(LOGIN_URL,
+            urllib.urlencode({'strEmail': settings.IBD_EMAIL,
+                'strPassword': settings.IBD_PASSWORD,
+                'blnRemember': 'False'}), headers=HEADERS)
+    response = urllib2.urlopen(req)
+    assert "SOK"in response.read()
+
+def get_stock(symbol):
+    req = urllib2.Request(URL + symbol, headers=HEADERS)
+    response = urllib2.urlopen(req)
+    return response
+
+def parse_investor_table(doc, title):
+    '''Helper for tables that contain a
+    <td><a..>{{title}}</a></td><td>{{value}}</td> structure.'''
+    title_element = doc.xpath("//td[contains(a, '%s')]" % title)[0]
+    if title_element is not None:
+        return title_element.getnext().text.strip()
+    return "N/A"
+
+def parse_stock(response):
+    '''This is the messy part. It parses the output using LXML and makes a
+    dictionary of values of interest. If there is breakage, its probably in
+    here.'''
+    parsed_data = {}
+    doc = lxml.html.parse(response).getroot()
+    company_group = doc.cssselect("#stock-checkup-group-performance strong")[0]
+    company_group = company_group.text.strip()
+    company_group_match = COMPANY_GROUP_RE.match(company_group)
+    parsed_data['company_name'], parsed_data['industry_group'] = \
+            company_group_match.groups()
+
+    parsed_data['industry_group_rank'] = parse_investor_table(doc,
+            'Industry Group Rank')
+
+    parsed_data['earnings_due_date'] = parse_investor_table(doc,
+            'EPS Due Date')
+
+    parsed_data['eps_rating'] = parse_investor_table(doc, 'EPS Rating')
+
+    parsed_data['eps_perc_change'] = parse_investor_table(doc, 
+            'EPS % Chg (Last Qtr)')
+
+    parsed_data['sales_perc_change'] = parse_investor_table(doc,
+            'Sales % Chg (Last Qtr)')
+    return parsed_data
