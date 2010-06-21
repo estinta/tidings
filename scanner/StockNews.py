43b4547f4a8e11beeaeee39e3d5f7c2683011db4
Dan McGee <dpmcgee@gmail.com>
2010-06-21 13:19:33 -0500

    Add yahoo RSS date parser
    This should help us get the right time zone on our news entries.

--- scanner/StockNews.py
+++ scanner/StockNews.py
@@ -1,14 +1,31 @@
 import feedparser
 import re
+import time
 
 URLS = {'Yahoo': "http://finance.yahoo.com/rss/headline?s=",
         'Google': "http://www.google.com/finance/company_news?output=rss&q=",
-        'MSN': "http://moneycentral.msn.com/community/rss/generate_feed.aspx?feedType=0&Symbol="}
+        'MSN': "http://moneycentral.msn.com/community/rss/generate_feed.aspx?feedType=0&Symbol="
+}
 USER_AGENT = "Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.1.2) Gecko/20090729 Firefox/3.5.2"
 
 HEADERS={'User-Agent': USER_AGENT}
 
 HTML_TAGS = re.compile('<.*?>')
+
+YAHOO_DATE = re.compile(r'([A-Za-z]{3}), (\d{,2}) ([A-Za-z]{3}) (\d{4}) (\d{,2}):(\d{2}):(\d{2}) Etc/GMT')
+
+def yahoo_date_parser(date_string):
+    '''They like inventing their own funky format, here it is:
+    Mon, 21 Jun 2010 17:15:35 Etc/GMT
+    '''
+    dow, day, month, year, hour, minute, second = \
+            YAHOO_DATE.search(date_string).groups()
+    cleaned = "%d %s %d %d:%d:%d" % (int(day), month, int(year),
+            int(hour), int(minute), int(second))
+    t = time.strptime(cleaned, "%d %b %Y %H:%M:%S")
+    print "used yahoo date formatter for", date_string
+    return t
+feedparser.registerDateHandler(yahoo_date_parser)
 
 def get_news(symbol):
     news = {}
