107cae6f6e5f2a1f35818df25dc23b8a0fc60803
Dan McGee <dpmcgee@gmail.com>
2010-08-09 10:24:47 -0500

    Ensure symbols are always uppercase
    This is the cause of many of our Django errors regarding duplicate keys.
    Thank you MySQL for this lovely piece of work.

--- reporter/forms.py
+++ reporter/forms.py
@@ -1,11 +1,15 @@
 from django import forms
 
+import re
+
 class BuildListForm(forms.Form):
     symbols = forms.CharField(widget=forms.Textarea, required=False,
             help_text="enter symbols, separated by spaces, tabs, and/or newlines")
-    #upload_file = forms.FileField(required=False,
-    #        help_text = "upload textfile containing symbols separated by whitespace")
 
+    def clean_symbols(self):
+        # ensure our symbols are normalized in some fashion
+        data = self.cleaned_data['symbols']
+        return data.upper()
 
 def process_build_list(request):
     '''
@@ -14,15 +18,23 @@ def process_build_list(request):
 	known list of stock symbols even if invoked without a form being present.
     '''
     symbols = request.session.get('symbols', set())
+
+    # backward-compatibility code- make sure all symbols are upper-case
+    correct_symbols = set()
+    for s in symbols:
+        correct_symbols.add(s.upper())
+    if symbols != correct_symbols:
+        request.session['symbols'] = correct_symbols
+        symbols = correct_symbols
+
     if 'build_list' not in request.POST:
         return symbols
 
     build_list_form = BuildListForm(data=request.POST, files=request.FILES)
     if build_list_form.is_valid():
-        cleaned = build_list_form.cleaned_data
-        symbols |= set(cleaned['symbols'].split())
-        #if cleaned['upload_file']:
-        #    symbols |= cleaned['upload_file'].read().split()
+        cleaned = build_list_form.cleaned_data['symbols']
+        new_symbols = re.findall(r'\w+', cleaned)
+        symbols |= set(new_symbols)
         request.session['symbols'] = symbols
 
     return symbols
