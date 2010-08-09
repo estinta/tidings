421e3ad1e41a373b78ddea042ad6c1446dffbce4
Dan McGee <dpmcgee@gmail.com>
2010-08-09 08:18:24 -0500

    Add time index to tweets

--- aggregator/models.py
+++ aggregator/models.py
@@ -4,4 +4,4 @@ from django.contrib.auth.models import User
 class Tweet(models.Model):
     user = models.ForeignKey(User)
     message = models.TextField()
-    time = models.DateTimeField(auto_now_add=True)
+    time = models.DateTimeField(auto_now_add=True, db_index=True)
